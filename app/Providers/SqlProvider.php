365d5b1e8279dcc1feb5318fefaa46b3eb61c526
frank <frankerzeng@163.com>
2016-09-07 16:11:29 +0800

    门面+服务提供者

---
+++ app/Providers/SqlProvider.php
@@ -0,0 +1,40 @@
+<?php
+//>php artisan make:provider SqlProvider
+//打印sql执行语句
+
+namespace App\Providers;
+
+use App\User_Bind;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class SqlProvider extends ServiceProvider {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    //打印sql执行语句
+    public function boot() {
+        DB::listen(function ($query) {
+            $f = fopen("./sql_log/" . date("Y-m-d", time()) . ".txt", "a+");
+            fwrite($f, $query->sql . "\n");
+            fclose($f);
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->app->bind('user_bind', function ($app) {
+            return new User_Bind();
+        });
+        // 单例
+//        $this->app::singleton('zeng', function () {
+//            return new User_Bind();
+//        });
+    }
+}
