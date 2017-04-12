ca2b45c13ed50a89188e6dffbb394c7bba9caa6b
frank <frankerzeng@163.com>
2017-04-12 15:22:12 +0800

    web hook

--- public/web_hook.php
+++ public/web_hook.php
@@ -8,9 +8,16 @@
 
 print_r(file_get_contents("php://input"));
 
-$request = file_get_contents("php://input");
-$content = json_decode($request);
+//$request = file_get_contents("php://input");
+//$content = json_decode($request);
 
+$rest = shell_exec("cd /usr/local/nginx/html/blog && git pull git@github.com:frankerzeng/laravel_bolg.git");
+$rest . PHP_EOL;
+
+$request = date("Y-m-d H:i:s") . "-----";
+$request .= $rest;
+
+$request .= "\n";
 file_put_contents("git-webhook.txt", $request, FILE_APPEND);
 
 echo 1111;
