94ebcd2b5cdb40017750d7105d7c61fdf6c3f77a
frank <frankerzeng@163.com>
2017-04-12 14:55:27 +0800

    web hook

--- public/web_hook.php
+++ public/web_hook.php
@@ -8,5 +8,10 @@
 
 print_r(file_get_contents("php://input"));
 
+$request = file_get_contents("php://input");
+$content = json_decode($request);
+
+file_put_contents("git-webhook.txt", $request, FILE_APPEND);
+
 echo 11;
 
